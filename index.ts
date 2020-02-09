41e29911987d112fa8243b60b31b4dfbcfdff1d0
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-09 19:24:59 +0100

    :heavy_plus_sign: Add .env

--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
+import { config } from "dotenv";
 import { readFile } from "fs-extra";
 import { load } from "js-yaml";
 import { join } from "path";
-import {} from "puppeteer";
+import { launch, Page } from "puppeteer";
+
+config();
 
 export const archive = async () => {
+  console.log("⏳ Starting archiving process...");
   const yarn = await readFile(join(__dirname, "..", "urls.yml"));
   const urls: string[] = load(yarn.toString());
-  for await (const url of urls) {
-    await archiveUrl(url);
-    console.log("✓ Archived", url);
-  }
+  const browser = await launch();
+  const page = await browser.newPage();
+  await login(page);
+  // for await (const url of urls) {
+  //   await archiveUrl(url, page);
+  //   console.log("✅", url);
+  // }
+  // await browser.close();
+  // console.log("✅ Archived all pages");
 };
 
-const archiveUrl = async (url: string) => {};
+const login = async (page: Page) => {};
+
+const archiveUrl = async (url: string, page: Page) => {
+  await page.goto("https://web.archive.org/save");
+  await page.type("input#web-save-url-input", url, { delay: 20 });
+};
