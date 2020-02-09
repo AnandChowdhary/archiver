68759dc72391c7b0f281ba3bcfcdb46e5fe996ee
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-09 19:09:42 +0100

    :sparkles: Add running from npm script

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+import { readFile } from "fs-extra";
+import { load } from "js-yaml";
+import { join } from "path";
 
+export const archive = async () => {
+  const yamlFile = await readFile(join(__dirname, "..", "urls.yml"));
+  const yaml = load(yamlFile.toString());
+  console.log("yaml", yaml);
+};
